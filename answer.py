48d9dd0519c33f00c5f46f55edc62f480484cb68
Kranti Sri Kalepalli <157083369+kkrantisri@users.noreply.github.com>
2024-09-14 21:56:33 -0400

    Initial commit

--- answer.py
+++ answer.py
@@ -1,17 +1,38 @@
-def hello_world():
-    # Write a doc string below. Doc strings can be used to provide users with a
-    # description of what the function does.
+
+
+def tuple():
     """
-
+    This is to review tuples and basic operations.
     """
+    # Assign the first element of the tuple "t1" to the variable x.
+    t1 = (13, 100, 99, 100, 999)
 
-    # Write a comment; comments are very useful for other programmers to understand.
 
-    # Write a Python statement to print Hello, World!
+    # Assign the next to the last element of the tuple "t1" to the
+    # variable y. For the given tuple, the next to the last element
+    # is 100.
 
-def add(a,b):
 
-    # Write simple code to add variables a and b
-    output =
+    # Determine the index of 2 in the tuple "t2" by using the index
+    # method for tuples. Assign the result to the variable z.
+    
+    t2 = (55, 777, 54, 6, 76, 101, 1, 2, 8679, 98, 123, 98, 98)
 
-    return output
+    # Determine the number of times 98 occurs in the tuple "t2" by
+    # using the count method for tuples. Assign the result to
+    # the variable f.
+    
+
+    # Determine the sum of all the numbers in the tuple "t2" and
+    # assign the result to the variable m.
+    
+
+    # Determine the maximum number of all the numbers
+    # in the tuple "t2" and assign the result to the variable n.
+
+
+    return x, y, z, f, m, n
+
+
+
+tuple()
